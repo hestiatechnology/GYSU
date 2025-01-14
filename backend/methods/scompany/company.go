230cf6ed8b883a651e6a744eec84f8f9e2b35aa0
danielalexis <danielalexis.o.pereira@gmail.com>
2025-01-14 13:33:45 +0000

    Add database model for company and implement database connection in Company service

--- backend/methods/scompany/company.go
+++ backend/methods/scompany/company.go
@@ -4,7 +4,11 @@ import (
 	"context"
 	"hestia/jobfair/api/pb/common"
 	"hestia/jobfair/api/pb/company"
+	"hestia/jobfair/api/utils/db"
+	"hestia/jobfair/api/utils/herror"
 
+	"github.com/rs/zerolog/log"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
@@ -16,6 +20,13 @@ func (CompanyServer) CreateCompany(ctx context.Context, c *company.Company) (*co
 	return nil, nil
 }
 func (CompanyServer) GetCompanies(ctx context.Context, emptypb *emptypb.Empty) (*company.ListCompaniesResponse, error) {
+	db, err := db.GetDBPoolConn()
+	if err != nil {
+		log.Err(err).Msg("Error getting db connection")
+		return nil, herror.StatusWithInfo(codes.Internal, "Error getting db connection", herror.DatabaseError, company.CompanyService_ServiceDesc.ServiceName, nil).Err()
+	}
+	defer db.Close()
+
 	return nil, nil
 }
 func (CompanyServer) GetCompany(ctx context.Context, id *common.Id) (*company.Company, error) {
